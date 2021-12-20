68128887d5093d9f268df64e7f0526cdba3f4f7d
dsevenx <50168945+dsevenx@users.noreply.github.com>
2021-12-20 21:51:09 +0100

    ohne Main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // eigene tech Komponenten
-import { Main } from "./Main";
+import { Datenschutz } from "./components/Datenschutz";
+import { Home } from "./components/cp_tech/Home";
+import { Impressum } from "./components/Impressum";
+import { Footer } from "./components/cp_tech/Footer";
 
 export default class App extends Component {
+  // jetzt render3
   render() {
     return (
-      <div>
-        <Main></Main>
+      <div className="page-container">
+        <div className="content-wrap">
+          <Container>
+            <Router>
+              <Routes>
+                <Route
+                  path="/planentenspieldatenschutz"
+                  element={<Datenschutz app_name="'PlanetenspielUndApp2b'" />}
+                />
+                <Route
+                  path="/buchstabenspieldatenschutz"
+                  element={<Datenschutz app_name="'buchstaben lernen app'" />}
+                />
+                <Route
+                  path="/kirisorganizer"
+                  element={<Datenschutz app_name="'KirisOrganizer'" />}
+                />
+                <Route path="/impressum" element={<Impressum />} />
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </Router>
+          </Container>
+        </div>
+        <Footer css_chen="main-footer-all" />
       </div>
     );
   }
